640171ee660b20159584cc4c42e78be282fd938e
Mathias Burger <mathias.burger@gmail.com>
2018-03-14 21:07:57 +0100

    process namespace

--- src/converter/convertGirToJs.js
+++ src/converter/convertGirToJs.js
@@ -1,7 +1,37 @@
 #!/usr/bin/env node
 
-const girFile = process.argv[1];
-const jsFile = process.argv[2];
+const
+    xml2js = require('xml2js'),
+    fs = require('fs'),
+    path = require('path');
+
+const girFile = process.argv[2];
+let jsFile = process.argv[3];
+if (!jsFile.startsWith("/")) {
+    jsFile = path.normalize(__dirname + "/../../" + jsFile);
+}
 
 console.log(girFile);
 console.log(jsFile);
+
+function processGir(gir) {
+    let converted = "";
+    const repository = gir['repository'];
+    repository['namespace'].forEach(function (namespace) {
+        const name = namespace.$.name;
+        converted += "var " + name + " = {};\n";
+    });
+    return converted;
+}
+
+parser = new xml2js.Parser();
+fs.readFile(girFile, function(err, data) {
+    parser.parseString(data, function (err, result) {
+        let converted = processGir(result);
+        fs.writeFile(jsFile, converted, function(err) {
+            if(err) {
+                console.log(err);
+            }
+        });
+    });
+});
