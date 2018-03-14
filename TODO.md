2517995e7e636dbacb3a404eea056c8a5a65eda4
Mathias Burger <mathias.burger@gmail.com>
2018-03-14 21:49:06 +0100

    pretty print generated  js before writing, add todos

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODOs
+
+Process elements in namespace
+* alias
+* bitfield
+* callback
+* class
+* constant
+* enumeration
+* function
+* interface
+* record
--- src/converter/convertGirToJs.js
+++ src/converter/convertGirToJs.js
@@ -3,7 +3,8 @@
 const
     xml2js = require('xml2js'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    beautify = require('js-beautify').js_beautify;
 
 const girFile = process.argv[2];
 let jsFile = process.argv[3];
@@ -19,7 +20,18 @@ function processGir(gir) {
     const repository = gir['repository'];
     repository['namespace'].forEach(function (namespace) {
         const name = namespace.$.name;
-        converted += "var " + name + " = {};\n";
+        converted += "var " + name + " = {"
+            + processClasses(namespace)
+            + "};\n";
+    });
+    return converted;
+}
+
+function processClasses(namespace) {
+    let converted = "";
+    namespace.class.forEach(function (clazz) {
+        const name = clazz.$.name;
+
     });
     return converted;
 }
@@ -28,7 +40,7 @@ parser = new xml2js.Parser();
 fs.readFile(girFile, function(err, data) {
     parser.parseString(data, function (err, result) {
         let converted = processGir(result);
-        fs.writeFile(jsFile, converted, function(err) {
+        fs.writeFile(jsFile, beautify(converted, {indent_size: 4}), function(err) {
             if(err) {
                 console.log(err);
             }
