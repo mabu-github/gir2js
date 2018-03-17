24364c678eb11db7d665b108909c0bfa1438c104
Mathias Burger <mathias.burger@gmail.com>
2018-03-17 19:07:54 +0100

    process gnome shell gir files as well

--- test/converter/convertGirFilesToJs.sh
+++ test/converter/convertGirFilesToJs.sh
@@ -2,6 +2,6 @@
 
 mydir=$(dirname $0)
 
-find /usr/share/gir-1.0 -name *.gir \
+cat <(find /usr/share/gir-1.0 -name *.gir) <(find /usr/share/gnome-shell -name *.gir) \
     | xargs -n1 -I % sh -c 'echo -n %; echo -n " "; echo -n "'$mydir'"/../../build/; echo -n $(basename % .gir); echo -n ".js"; echo' \
     | xargs -n2 "$mydir"/../../src/converter/convertGirToJs.js
